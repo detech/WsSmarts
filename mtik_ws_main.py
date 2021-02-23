ca3488995ca91683ff423fa177e210e70a74283d
Kaleem <deepembtech@gmail.com>
2021-02-22 20:08:16 -0800

    the basic program that we checked in the meeting to getch the data related to a candle stick for a stock over a period of time. The data is pulled form yahoo finance and saved in data folder in a CSV file. checking in one .csv file as a sample

--- mtik_ws_main.py
+++ mtik_ws_main.py
@@ -2,20 +2,26 @@ from pandas_datareader import data as pdr
 import yfinance as yf
 yf.pdr_override()
 import pandas as pd
+
 from datetime import date
+import os
 
 #Reference: https://medium.com/@jouneidraza522/yahoo-finance-api-to-get-stocks-tickers-data-in-python-c49820249a18
 #https://www.youtube.com/watch?v=eSpH6fPd5Yw
 
 # Stock tickers for which data will be fetched
-#ticker_list=['DJIA', 'DOW', 'LB', 'EXPE', 'PXD', 'MCHP', 'CRM', 'JEC', 'NRG', 'HFC', 'NOW']
-ticker_list=['DJIA', 'DOW']
+ticker_list=['DJIA', 'DOW', 'CCIV', 'AAPL']
+
 today=date.today()
 files=[]
 
+outdir = './data'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
 #Specify historical price date range
-start_date="2020-12-01"
-end_date="2021-02-01"
+start_date="2021-02-01"
+end_date="2021-02-18"
 
 #Method to get the ticker data and save to a file
 def getData(ticker):
@@ -35,7 +41,8 @@ def printDataSummary():
     for i in range(0,len(ticker_list)):
         print('reading from ' + './data/'+str(files[i])+'.csv')
         df1=pd.read_csv('./data/'+str(files[i])+'.csv')
-        print(df1.head())
+        print(df1)
+        # print(df1.head())
 
 for tik in ticker_list:
     getData(tik)
